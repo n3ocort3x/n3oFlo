5483b0ea3b8b78133568fe90487d1ad30d62c002
n3ocort3x <peter.hofinger@gmail.com>
2013-09-14 21:01:49 +0200

    cpufreq: hardcode min/max freq

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -211,13 +211,13 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 
 	if (cpufreq_frequency_table_cpuinfo(policy, table)) {
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
-		policy->cpuinfo.min_freq = CONFIG_MSM_CPU_FREQ_MIN;
-		policy->cpuinfo.max_freq = CONFIG_MSM_CPU_FREQ_MAX;
+		policy->cpuinfo.min_freq = 384000;
+		policy->cpuinfo.max_freq = 1998000;
 #endif
 	}
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
-	policy->min = CONFIG_MSM_CPU_FREQ_MIN;
-	policy->max = CONFIG_MSM_CPU_FREQ_MAX;
+	policy->min = 384000;
+	policy->max = 1998000;
 #endif
 
 	cur_freq = acpuclk_get_rate(policy->cpu);
